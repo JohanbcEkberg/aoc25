cb196c679f8905fa04cc6d35c9c6b2c451d5a4f5
JohanBCEkberg <johanbcekberg@gmail.com>
2025-12-02 09:59:44 +0100

    Part 2

--- day02/solution.py
+++ day02/solution.py
@@ -18,8 +18,33 @@ def part1():
 
   return sum(ids)
 
+def split_chunks(s, n):
+  chunk_set = set()
+  for i in range(0, len(s), n):
+    chunk_set.add(s[i:i+n])
+  return chunk_set
+
+def is_valid_id_part_2(num):
+  s = str(num)
+  for i in range(1, len(s)):
+    if len(s) % i == 0:
+      chunks = split_chunks(s, i)
+      if len(chunks) == 1:
+        return True
+  return False
+
+
 def part2():
-  pass
+  data = read_input("input")
+  ids = []
+  for item in data:
+    low, high = map(int, item.split("-"))
+
+    for num in range(low, high + 1):
+      if is_valid_id_part_2(num):
+        ids.append(num)
+
+  return sum(ids)
 
 if __name__ == "__main__":
   print("Part 1:", part1())
