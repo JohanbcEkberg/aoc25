83e9ec498e5ced47a0734ca3dc0c3184c18fe45f
JohanBCEkberg <johanbcekberg@gmail.com>
2025-12-07 09:45:33 +0100

    Part 2

--- day07/solution.py
+++ day07/solution.py
@@ -14,9 +14,9 @@ def rec(matrix: list[list[str]], row: int, col: int) -> int:
   elif matrix[row][col] == '|':
     return 0
   elif matrix[row][col] == '^':
-    return rec(matrix, row + 1, col + 1) + rec(matrix, row + 1, col - 1) + 1
+    return rec(matrix, row, col + 1) + rec(matrix, row, col - 1) + 1
   else:
-    raise ValueError("Value of matrix[row][col] is ", matrix[row][col])
+    raise ValueError("Value of matrix[row][col] is " + matrix[row][col])
 
 def part1():
   matrix = read_input("./input")
@@ -26,8 +26,39 @@ def part1():
 
   return rec(matrix, 1, idx)
 
+def rec2(matrix: list[list[str]], row: int, col: int, memo: dict) -> int:
+  while row < len(matrix) and matrix[row][col] == '.':
+    matrix[row][col] = '|'
+    row += 1
+
+  mem_key = (row, col)
+
+  if mem_key in memo:
+    return memo[mem_key]
+
+  res = 0
+  if row == len(matrix):
+    res = 1
+  elif matrix[row][col] == '|':
+    res = rec2(matrix, row + 1, col, memo)
+  elif matrix[row][col] == '^':
+    res = rec2(matrix, row, col + 1, memo) + rec2(matrix, row, col - 1, memo)
+  else:
+    raise ValueError("Value of matrix[row][col] is " + matrix[row][col])
+
+  memo[mem_key] = res
+  return res
+  
 def part2():
-  pass
+  matrix = read_input("./input")
+  idx = 0
+  while matrix[0][idx] != 'S':
+    idx += 1
+
+  memo = {}
+  res = rec2(matrix, 1, idx, memo)
+
+  return res
 
 if __name__ == "__main__":
   print(f"Part 1: {part1()}")
