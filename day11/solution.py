1c6e15c714e2df931276869c68898ad12e356328
JohanBCEkberg <johanbcekberg@gmail.com>
2025-12-11 07:07:41 +0100

    Part 2

--- day11/solution.py
+++ day11/solution.py
@@ -21,10 +21,32 @@ def all_paths_from_you_to_out(graph, current='you', seen=set()):
 
 def part1():
   data = read_input("./input")
-  return all_paths_from_you_to_out(data)
+  return all_paths_from_you_to_out(data)    
+
+def all_paths_with_fft_and_dac(graph, node, seen_fft=False, seen_dac=False, memo={}):
+  key = (node, seen_fft, seen_dac)
+  if node == "out":
+    memo[key] = 1 if (seen_fft and seen_dac) else 0
+    return memo[key]
+
+  if node == "fft":
+    seen_fft = True
+  if node == "dac":
+    seen_dac = True
+
+  if key in memo:
+    return memo[key]
+
+  total = 0
+  for nxt in graph.get(node, []):
+    total += all_paths_with_fft_and_dac(graph, nxt, seen_fft, seen_dac, memo)
+
+  memo[key] = total
+  return total
 
 def part2():
-  pass
+  data = read_input("./input")
+  return all_paths_with_fft_and_dac(data, "svr")
 
 if __name__ == "__main__":
   print("Part 1:", part1())
